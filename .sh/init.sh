0fb6d262c8b3812645ceb8a7099a65d58a2ceb03
lawbh93/VlocityCICD <VlocityCICD@noreply.github.com>
2022-01-05 17:48:17 -0600

    Feature

--- .sh/init.sh
+++ .sh/init.sh
@@ -7,6 +7,33 @@ for ((i = 0; i < ${#protectedBranches[@]}; ++i)); do
 
 if [[ " ${protectedBranches[i]} " =~  ${Destino} ]]; then
        VAR=$(git diff remotes/origin/$Destino...$Origen --name-only)
-       echo $VAR
 fi
 done
+
+echo 'Differences start'
+echo $VAR
+echo 'Differences end'
+
+git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
+git fetch --all
+git branch --show-current
+
+node index/FindStrings.js $VAR
+
+git add tmpDatapacks/
+#
+for ((i = 0; i < ${#protectedBranches[@]}; ++i)); do
+
+if [[ " ${protectedBranches[i]} " =~  ${Destino} ]]; then
+       VAR2=$(git diff --cached --name-only $Origen tmpDatapacks)
+fi
+done
+
+echo 'Differences start2'
+echo $VAR2 
+echo 'Differences end2'
+node index/FindStringC.js $VAR2
+git add index/
+git restore --staged tmpDatapacks
+git status
+git diff --cached $BRANCH index
