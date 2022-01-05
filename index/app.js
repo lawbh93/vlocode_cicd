548b481010f89f84389196068ccce16cfa6af15a
lawbh93/VlocityCICD <VlocityCICD@noreply.github.com>
2022-01-05 17:59:41 -0600

    Remove aditional Data

--- index/app.js
+++
@@ -1,8 +0,0 @@
-var execProcess = require("./exec_process.js");
-execProcess.result("bash temp.sh", function(err, response){
-    if(!err){
-        console.log(response);
-    }else {
-        console.log(err);
-    }
-});
