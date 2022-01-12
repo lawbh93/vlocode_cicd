b8e7b33922f263c31e8371e85afce8ece3eb2b73
lawbh93/VlocityCICD <VlocityCICD@noreply.github.com>
2022-01-12 13:30:23 -0600

    try 1 before update secrets

--- .sh/oAuth.sh
+++ .sh/oAuth.sh
@@ -3,12 +3,19 @@ echo 'Here'
 action=${action:default}
 defURL=${defURL:default}
 getOrgInfoVar='getOrgInfo'
+oAuthInfoVar='oAuthInfo';
 username=${username:default}
 client_secret=${client_secret:default}
 passToken=${passToken:default}
 client_id=${client_id:default}
+access_token=${access_token:default}
+instance_url=${instance_url:default}
+OrgId=${OrgId:default}
+
 
 if [[ " ${action} " =~  ${getOrgInfoVar} ]]; then
   curl -d "username=$username&client_secret=$client_secret&password=$passToken&grant_type=password&client_id=$client_id" -H "Accept: application/json" $defURL -o accessInfo.json
-  cat ./accessInfo.json
+
+elif  [[ " ${action} " =~  ${getOrgInfoVar} ]]; then
+vlocity -sf.accessToken $access_token -sf.instanceUrl $instance_url -sf.sessionId $OrgId -vlocity.namespace vlocity_cmt -job ./index/datapack.yaml cleanOrgData
 fi
---
+++ index/oAuthVlocity.js
@@ -0,0 +1,29 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+var fs = require('fs');
+var fse = require('fs-extra');
+var action='oAuthInfo';
+var OrgId='';
+process.argv.forEach((val, index) => {
+    OrgId=val;
+});
+
+fs.readFile('./accessInfo.json', 'utf8', (err2, jsonString) => {
+    try {
+        const oAuthInfo = JSON.parse(jsonString);
+        ;
+        oAuthInfo.instance_url;
+          var execProcess = require("./exec_process.js");
+          execProcess.result(`access_token=${oAuthInfo.access_token} instance_url=${oAuthInfo.instance_url} action=${action} OrgId=${OrgId}  bash .sh/oAuth.sh`, function(err, response) {
+              if(!err){
+                  console.log(response);
+              }else {
+                  console.log(err);
+              }
+          });
+    
+          
+} catch(err2) {
+        console.log('Error parsing JSON string 2:', err2)
+    }
+})
