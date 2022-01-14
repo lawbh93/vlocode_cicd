1582efc1b70c87c04eafc2ace625fe7c65c0bfe3
lawbh93/VlocityCICD <VlocityCICD@noreply.github.com>
2022-01-14 14:53:28 -0600

    fix

--- index/exec_process.js
+++ index/exec_process.js
@@ -1,11 +1,13 @@
 var exec = require('child_process').exec;
-
+const core = require("@actions/core");
 var result = function(command, cb){
     var child = exec(command, function(err, stdout, stderr){
         if(err != null){
             return cb(new Error(err), null);
+            core.setFailed(err.message);
         }else if(typeof(stderr) != "string"){
             return cb(new Error(stderr), null);
+            core.setFailed(err.message);
         }else{
             return cb(null, stdout);
         }
